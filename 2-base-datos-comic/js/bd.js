4a78e9600cb82ee78816f6defd20bcc639b2d938
juanLS07 <juanjose789t@gmail.com>
2025-11-06 15:20:24 -0500

    base de datos comic

---
+++ 2-base-datos-comic/js/bd.js
@@ -0,0 +1,32 @@
+let comic = {
+  "nombreComic": "NarraMentum",
+  "sinopsis": "Un cómic que narra historias de la epoca industrial con un toque de fantasía y aventura.",
+  "year": 2025,
+  "autores": ["Juan Lopera", "Diego Alonso"],
+  "personajes": ["James Watt", "Eloy", "Ninove", "Mineros"],
+  "genero": "ficcion",
+  "portada": "./images/portada.jpg",
+  "escenas": [
+    {
+      "id": 1,
+      "nombre": "Dia de mina",
+      "personajes": ["Mineros", "Eloy"],
+      "image": "./images/escena1.jpg",
+    },
+   
+    {
+      "id": 2,
+      "nombre": "encontrar los objetos",
+      "personajes": ["Mineros", "Eloy"],
+      "image": "./images/escena2.jpg",
+    },
+   
+    {
+      "id": 3,
+      "nombre": "ninove",
+      "personajes": ["James Watt", "Ninove"],
+      "image": "./images/escena3.jpg",
+    },
+   
+  ]
+}
